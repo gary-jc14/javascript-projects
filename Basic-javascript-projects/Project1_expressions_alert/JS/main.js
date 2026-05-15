a1e3c725a4a4b6db87adcc31282542455bddbd9e
Gary <gjcairns1@gmail.com>
2026-05-15 18:12:40 +0100

    updated .js file

--- Basic-javascript-projects/Project1_expressions_alert/JS/main.js
+++ Basic-javascript-projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,13 @@
-window.alert ("Hello, World!");
-document.write ("Hello, World!");
+window.alert ("Hello, World!"); // This is an alert message that will pop up when the page loads
+document.write ("\"I\'m 50 years old\", said Gary!"); //
+document.write("<br>"); //
+document.write ("\"Be who you are and say what you feel, " + "because those who mind don\'t matter and those who matter don\'t mind.\" - Dr. Seuss"); // This is a quote that will be displayed on the page
+document.write("<br>"); //
+var Family = "The Cairns\'s", Dad = "Gary", Mom = "Jenny", Son = "Freddie", Brother = "Ari"; //
+document.write (Dad); // This will display the value of the variable Dad, which is "Gary"
+document.write("<br>"); // This will create another line break on the page
+document.write (25 + 25); // This will display the result of the expression 25 + 25, which is 50
+document.write("<br>");
+var Sent1 = "This is the beggining of the string";
+var Sent2 = " and this is the end of the string.";
+document.write (Sent1 + Sent2); // This will concatenate the two strings Sent1 and Sent2 and display the result on the page
